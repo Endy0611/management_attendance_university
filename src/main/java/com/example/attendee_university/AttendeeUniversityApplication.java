48bb5f19696b8a7c9509dc05ca0d0897807e9e62
Endy0611 <endyong18@gmail.com>
2026-06-25 13:22:41 +0700

    upate port

--- src/main/java/com/example/attendee_university/AttendeeUniversityApplication.java
+++ src/main/java/com/example/attendee_university/AttendeeUniversityApplication.java
@@ -29,7 +29,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
                                 """),
         servers = {
                 @Server(url = "http://localhost:9090", description = "Local Development Server"),
-                @Server(url = "http://localhost:8080", description = "Local Development Server")
+                @Server(url = "http://localhost:8080", description = "Local Development Server"),
+                @Server(url = "http://34.21.226.32:8080", description = "Local Development Server"),
         })
 @SecurityScheme(
         name = "bearerAuth",
