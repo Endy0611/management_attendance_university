fc2779698694f475741606dda3e2d8d093415404
Endy0611 <endyong18@gmail.com>
2026-07-05 22:26:12 +0700

    refactor: webmvcconfig

--- src/main/java/com/example/attendee_university/config/CorsConfig.java
+++ src/main/java/com/example/attendee_university/config/CorsConfig.java
@@ -1,25 +1,30 @@
 package com.example.attendee_university.config;
-import org.springframework.context.annotation.Bean;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+@RequiredArgsConstructor
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry
-                        .addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","http://localhost:9090", "https://instantcheck.online/", "https://instantcheck.online")
-                        .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
+    private final RateLimitInterceptor rateLimitInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:9090", "https://instantcheck.online/", "https://instantcheck.online")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor);
     }
 }
